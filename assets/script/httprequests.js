87ed4e871113c4122c482891392baf8d654b1db3
nurlanahasanzada17 <6ea7n3z@code.edu.az>
2023-04-30 19:25:15 +0400

    hello

---
+++ assets/script/httprequests.js
@@ -0,0 +1,60 @@
+const API_URL = 'https://jsonplaceholder.typicode.com/users';
+
+// GET ALL POSTS
+export const getAllPosts = async () => {
+    let globalData;
+    await fetch(`${baseURL}/posts`)
+        .then(res => res.json())
+        .then(data => {
+            globalData = data;
+        })
+    return globalData;
+}
+
+
+// GET POSTS BY ID
+export const getPostsById = async (id) => {
+    let globalData;
+    await fetch(`${baseURL}/posts/${id}`)
+        .then(res => res.json())
+        .then(data => {
+            globalData = data;
+        })
+    return globalData;
+}
+
+// DELETE POSTS BY ID
+export const deletePostsById = async (id) => {
+    await fetch(`${baseURL}/posts/${id}`, {
+        method: `DELETE`,
+    })
+};
+
+// POST 
+export const post = async (post) => {
+    let globalData;
+    await fetch(`${baseURL}/posts/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(post)
+        })
+        .then(res => res.json())
+        .then(data => {
+            globalData = data;
+        })
+    return globalData;
+}
+
+// PUT POSTS BY ID
+export const pustPosts = async (id, post) => {
+    let globalData;
+    await fetch(`${baseURL}/posts/${id}`, {
+        method: 'PuT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(post)
+    })
+}
--- assets/script/main.js
+++ assets/script/main.js
@@ -1,52 +1,47 @@
-const API_URL = 'https://jsonplaceholder.typicode.com/posts';
+// import {
+//     deletePostsById,
+// } from "./httprequests.js"; 
+
+
+
+const API_URL = 'https://jsonplaceholder.typicode.com/users';
+let spinner = document.querySelector(".spinner");
+
+spinner.style.display = "none";
 
 (async function getData() {
     const row = document.querySelector(".row");
+
+    spinner.style.display = "flex";
 
     try {
         const res = await fetch(API_URL)
         const data = await res.json()
 
         data.forEach(element => {
+
+            spinner.style.display = "none";
             let newCard = createCard(element);
             row.append(newCard);
         });
 
-        // select.addEventListener("change", function () {
-        //     row.innerHTML = "";
-
-        //     if (this.value == "id") {
-        //         data.sort((a, b) => a.id - b.id)
-        //         data.forEach(elem => {
-        //             row.append(createCard(elem))
-        //         })
-        //     } else if (this.value == "username") {
-        //         data.sort((a, b) => a.username.localeCompare(b.username))
-        //         data.forEach(elem => {
-        //             row.append(createCard(elem))
-        //         })
-        //     } else {
-        //         row.innerHTML = "adam ol"
-        //     }
-
-
-        // })
+        search.addEventListener("keyup", (e) => {
+            row.innerHTML = "";
+            let filteredData = data.filter((user) =>
+                user.name
+                .trim()
+                .toLowerCase()
+                .icludes(e.target.value.trim().toLowerCase())
+            );
+            filteredData.forEach((user) => {
+                let newCard = createCard(user);
+                row.append(newCard);
+            });
+        });
     } catch (err) {
         console.log(err)
     }
-
-
-
 })();
-
-// function myFunction() {
-//     let nav = document.getElementById("page");
-//     if (nav.className === "page") {
-//       nav.className += " responsive";
-//     } else {
-//       nav.className = "page";
-//     }
-//   }
 
 
 function createCard(user) {
@@ -60,16 +55,98 @@ function createCard(user) {
     let cardBody = document.createElement("div");
     cardBody.classList.add("card-body");
 
-    let title = document.createElement("h5");
-    title.classList.add("card-title");
-    title.innerText = `${user.title}`;
+    let image = document.createElement("img");
+    image.setAttribute("src", "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80");
 
-    let body = document.createElement("p");
-    body.classList.add("card-list");
-    body.innerText = `${user.body}`;
+    let name = document.createElement("h5");
+    name.classList.add("card-title");
+    name.innerText = `${user.name}`;
 
-    cardBody.append(title, body);
-    card.append(cardBody);
+    let username = document.createElement("p");
+    username.classList.add("card-list");
+    username.innerText = `${user.username}`;
+
+    let email = document.createElement("p");
+    email.classList.add("card-list");
+    email.innerText = `${user.email}`;
+
+    let address = document.createElement("p");
+    address.classList.add("card-list");
+    address.innerText = `${user.address.city}- ${user.address.street}`;
+
+    let phone = document.createElement("p");
+    phone.classList.add("card-list");
+    phone.innerText = `${user.phone}`;
+
+    let buttonDiv = document.createElement("div")
+    buttonDiv.classList.add("buttonDiv");
+
+    let editButton = document.createElement("button");
+    editButton.setAttribute("href", "home.html");
+    editButton.classList.add("button");
+    editButton.classList.add("btn-primary");
+    editButton.innerText = "Edit";
+
+    editButton.addEventListener("click", function () {
+        localStorage.setItem("currentCardDetails", JSON.stringify((user)));
+
+    });
+
+    let deleteButton = document.createElement("button");
+    deleteButton.setAttribute("data-id", `${user.id}`);
+    deleteButton.classList.add("button");
+    deleteButton.classList.add("btn-primary");
+    deleteButton.innerText = "Delete"
+
+    deleteButton.addEventListener("click", (e) => {
+
+        const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+                confirmButton: 'btn btn-success',
+                cancelButton: 'btn btn-danger'
+            },
+            buttonsStyling: false
+        })
+
+        swalWithBootstrapButtons.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, delete it!',
+            cancelButtonText: 'No, cancel!',
+            reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+                swalWithBootstrapButtons.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success'
+                );
+
+                let id = e.target.getAttribute("data-id")
+                deletePostsById(id);
+                e.target.parentElement.parentElement.parentElement.parentElement.remove();
+
+            } else if (
+                /* Read more about handling dismissals below */
+                result.dismiss === Swal.DismissReason.cancel
+            ) {
+                swalWithBootstrapButtons.fire(
+                    'Cancelled',
+                    'Your imaginary file is safe :)',
+                    'error'
+                )
+            }
+        })
+    })
+
+    buttonDiv.append(editButton, deleteButton)
+
+
+
+    cardBody.append(name, username, email, address, phone, buttonDiv);
+    card.append(image, cardBody);
     col.append(card);
 
     return col;
